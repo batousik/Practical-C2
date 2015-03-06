e65033a63f3e4fc5f10ed352648f0efac6d1a379
Bato-Bair Tsyrenov <batousik@yandex.ru>
2015-03-06 04:34:33 +0000

    [TEST] Running main for insert procedure

--- src/practical_trees.c
+++ src/practical_trees.c
@@ -1,18 +1,23 @@
 #include "treenode.h"
 
 int main(){
-	int a,b,c,d,e;
+	int a,b,c,d,e,f,g;
 	a=1;
 	b=2;
 	c=3;
 	d=4;
 	e=5;
+	f=98;
+	g=97;
 	TreeBase *tree_base = new_base(comp_ints, clean_int, print_int);
 	tree_base->base = new_node(&c);
 	tree_base->base->left = new_node(&b);
 	tree_base->base->right = new_node(&d);
 	tree_base->base->left->left = new_node(&a);
 	tree_base->base->right->right = new_node(&e);
+	insert(tree_base, f);
+	insert(tree_base, g);
+
 	printTree(tree_base);
 	return 0;
 }
