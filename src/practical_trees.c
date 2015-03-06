50a48dfef996348682b2f9009c82d7d066cc8ef8
Bato-Bair Tsyrenov <batousik@yandex.ru>
2015-03-06 04:07:04 +0000

    [FIX] Actual program fix

--- src/practical_trees.c
+++ src/practical_trees.c
@@ -1,12 +1,18 @@
 #include "treenode.h"
 
 int main(){
-	int x = 7;
+	int a,b,c,d,e;
+	a=1;
+	b=2;
+	c=3;
+	d=4;
+	e=5;
 	TreeBase *tree_base = new_base(comp_ints, clean_int, print_int);
-	tree_base->base->value = &x;
-	tree_base->base->left = tree_base->base;
-	tree_base->base->right = tree_base->base;
-	
+	tree_base->base = new_node(&c);
+	tree_base->base->left = new_node(&b);
+	tree_base->base->right = new_node(&d);
+	tree_base->base->left->left = new_node(&a);
+	tree_base->base->right->right = new_node(&e);
 	printTree(tree_base);
 	return 0;
 }
