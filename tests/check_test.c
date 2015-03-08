fae7f28884da2dc72c83e5b3172d0de912a19748
Bato-Bair Tsyrenov <batousik@yandex.ru>
2015-03-08 12:00:38 +0000

    [FIX]                                                           test6

--- tests/check_test.c
+++ tests/check_test.c
@@ -168,7 +168,9 @@ START_TEST(test_START_EMPTY_TREE_TREEBASE_PRINT_FREETREE_TEST) {
             ptr_helper = malloc(sizeof(int));
             if (!ptr_helper)
                 ck_abort_msg("FAILED memory allocation\n");
-            *ptr_helper = *(int_arr_ptr + i);
+            printf("%d\n",*(int_arr_ptr + i));
+            fflush(stdout);
+            //*ptr_helper =
             isValid = insert(ptr_tree_base_int_1, ptr_helper);
             ck_assert_int_eq(isValid, true);
         }
